d58a87bc6aba3a6ef0de056fa1d78bfa4c84c564
isiaburto <163420116+isiaburto@users.noreply.github.com>
2024-09-18 16:15:11 -0300

    Add files via upload

---
+++ Funciones.py
@@ -0,0 +1,17 @@
+from sympy import *
+init_printing(use_unicode=True)
+
+def EnsamblarBf(XYZ, IEN):
+    return
+
+def EnsamblarLr(XYZ, Ap):
+    return
+
+def EnsamblarF(XYZ, Fext):
+    return
+
+def Solver(XYZ, IEN, Ap, Fext):
+    return
+
+A=Matrix([[-1,1,1],[-2,-3,1],[3,1,-2]])
+pprint(A)
